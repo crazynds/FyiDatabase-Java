f2bba95cac4c9bb7a8e25c67999938ea5d84fff4
Crazynds <luli.broch@gmail.com>
2023-03-30 17:17:58 -0300

    fix: corrige right join a ordem dos parametros no comparator.

--- src/sgbd/query/binaryop/joins/RightNestedLoopJoin.java
+++ src/sgbd/query/binaryop/joins/RightNestedLoopJoin.java
@@ -6,7 +6,7 @@ import sgbd.util.ComparableFilter;
 
 public class RightNestedLoopJoin extends LeftNestedLoopJoin{
     public RightNestedLoopJoin(Operator left, Operator right, ComparableFilter<Tuple> comparator) {
-        super(right, left, comparator); // apenas inverte os operadores
+        super(right, left, (t1, t2) -> comparator.match(t2,t1)); // apenas inverte os operadores
     }
     public RightNestedLoopJoin(Operator left, Operator right) {
         super(right, left); // apenas inverte os operadores
