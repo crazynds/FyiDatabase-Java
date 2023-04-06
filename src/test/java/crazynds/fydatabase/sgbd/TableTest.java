79e10e7b247c3869dc0a976ec7c136c41100aebe
Crazynds <luli.broch@gmail.com>
2023-04-06 15:17:44 -0300

    refactor: agora o projeto esta como um projeto maven oficia

---
+++ src/test/java/crazynds/fydatabase/sgbd/TableTest.java
@@ -0,0 +1,66 @@
+package crazynds.fydatabase.sgbd;
+
+import org.junit.Test;
+import sgbd.prototype.Column;
+import sgbd.prototype.Prototype;
+import sgbd.table.Table;
+import sgbd.table.components.Header;
+import sgbd.test.ConsistenceTest;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertTrue;
+
+public class TableTest {
+
+    private void testConsistence(String tableType){
+        HashMap<String,String> mapa = new HashMap<>();
+        Prototype p1 = new Prototype();
+        p1.addColumn("id",4, Column.PRIMARY_KEY);
+        mapa.put("id","int");
+        p1.addColumn("nome",255,Column.DINAMIC_COLUMN_SIZE);
+        mapa.put("nome","string");
+        p1.addColumn("idade",4,Column.CAN_NULL_COLUMN);
+        mapa.put("idade","int");
+        p1.addColumn("salario",4,Column.FLOATING_POINT);
+        mapa.put("salario","float");
+        Header h = new Header(p1,"users");
+        h.set(Header.TABLE_TYPE,tableType);
+
+        Table users = Table.openTable(h);
+
+        ConsistenceTest consistenceTest = new ConsistenceTest(users,mapa,156);
+        int qtdData=1000;
+        int block = 10;
+        long startTime = System.nanoTime();
+        consistenceTest.generateRandomDataBlock(qtdData,block);
+        long generateTime = System.nanoTime();
+        assertTrue(consistenceTest.checkConsistence(qtdData));
+    }
+
+
+    @Test
+    public void testConsistenceSimpleTable(){
+        this.testConsistence("SimpleTable");
+    }
+
+//    @Test
+//    public void testConsistenceDoubleTable(){
+//        this.testConsistence("DoubleTable");
+//    }
+
+    @Test
+    public void testConsistenceMemoryBTreeTable(){
+        this.testConsistence("MemoryTable");
+    }
+
+    @Test
+    public void testConsistenceBTreeTable(){
+        this.testConsistence("BTreeTable");
+    }
+
+//    @Test
+//    public void testConsistenceBTreeDoubleTable(){
+//        this.testConsistence("BTreeDoubleTable");
+//    }
+}
