79427740a0d5ac1daa2d06e2da822d53c4bb47d4
Crazynds <luli.broch@gmail.com>
2023-06-22 17:45:48 -0300

    feat: criado class schema

---
+++ src/main/java/sgbd/prototype/query/Fields.java
@@ -0,0 +1,17 @@
+package sgbd.prototype.query;
+
+import sgbd.prototype.Column;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Fields implements Iterable<Map.Entry<String, Column>> {
+
+    private HashMap<String,Column> map = new HashMap<>();
+
+    @Override
+    public Iterator<Map.Entry<String, Column>> iterator() {
+        return map.entrySet().iterator();
+    }
+}
---
+++ src/main/java/sgbd/prototype/query/OperatorSchema.java
@@ -0,0 +1,20 @@
+package sgbd.prototype.query;
+
+import sgbd.prototype.Column;
+
+import java.util.*;
+
+public class OperatorSchema implements Iterable<Map.Entry<String,Fields>> {
+
+    private HashMap<String,Fields> map;
+
+    public OperatorSchema(){
+        map = new HashMap<>();
+    }
+
+
+    @Override
+    public Iterator<Map.Entry<String, Fields>> iterator() {
+        return map.entrySet().iterator();
+    }
+}
