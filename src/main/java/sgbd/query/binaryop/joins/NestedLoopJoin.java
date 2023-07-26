590cd17470a0901f0c7c6942cccff369ccb669dc
Crazynds <lh.lagonds@gmail.com>
2023-07-26 19:10:14 -0300

    fix: removido uso do boolean expression

--- src/main/java/sgbd/query/binaryop/joins/NestedLoopJoin.java
+++ src/main/java/sgbd/query/binaryop/joins/NestedLoopJoin.java
@@ -9,17 +9,27 @@ import sgbd.prototype.query.Tuple;
 import sgbd.query.binaryop.SimpleBinaryOperator;
 import sgbd.util.interfaces.ComparableFilter;
 
+import java.util.Comparator;
+
 public class NestedLoopJoin extends SimpleBinaryOperator {
     protected Tuple currentLeftTuple=null;
     protected BooleanExpression expression;
+    protected ComparableFilter<Tuple> comparable;
 
     public NestedLoopJoin(Operator left, Operator right) {
         super(left, right);
         this.expression = null;
+        this.comparable = null;
     }
     public NestedLoopJoin(Operator left, Operator right, BooleanExpression expression) {
         super(left, right);
         this.expression = expression;
+        this.comparable = null;
+    }
+    public NestedLoopJoin(Operator left, Operator right, ComparableFilter<Tuple> comparable) {
+        super(left, right);
+        this.expression = null;
+        this.comparable = comparable;
     }
 
     @Override
@@ -42,7 +52,10 @@ public class NestedLoopJoin extends SimpleBinaryOperator {
                 Tuple rightTuple = right.next();
                 //Faz a comparação do join
                 Query.COMPARE_JOIN++;
-                if(expression==null || expression.solve() == Result.TRUE){
+                //if(expression==null || expression.solve() == Result.TRUE){
+                //    return new Tuple(currentLeftTuple,rightTuple);
+                //}
+                if(comparable==null || comparable.match(currentLeftTuple,rightTuple)){
                     return new Tuple(currentLeftTuple,rightTuple);
                 }
             }
