b47d74f4eca7f828057cb75aa08d82746a5bc3af
Matheus Delazeri <matheusdelazeris@gmail.com>
2023-11-08 00:55:25 -0300

    Fix: Verifica se o header da source possui prototype antes the validar as colunas, permitindo header com prototype inicialmente nulo (JDBC Tables)

--- src/main/java/sgbd/source/Source.java
+++ src/main/java/sgbd/source/Source.java
@@ -17,7 +17,10 @@ public abstract class Source<T> implements Iterable<RowData>{
 
 
     public Source(Header header)  {
-        translatorApi = header.getPrototype().validateColumns();
+        if (header.getPrototype() != null) {
+            translatorApi = header.getPrototype().validateColumns();
+        }
+
         this.header = header;
     }
 
